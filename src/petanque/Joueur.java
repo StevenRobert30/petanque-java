5096b611b58f83f18c788451ce9a6acb913f0546
Steven Robert <contact.stevenrobert@gmail.com>
2021-11-19 15:44:06 +0000

    test push git

---
+++ src/petanque/Joueur.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package petanque;
+
+/**
+ *
+ * @author steve
+ */
+public class Joueur {
+     //Attributs
+    private String nom;
+    private String prenom;
+    private int age;
+
+
+    /**
+     * Constructeur implicite suite à masquage
+     * */
+    public Joueur() {
+        
+    }
+    
+    /**
+     * Créer un joueur aléatoire
+     * */
+    public void generateRandomJoueur() {
+        
+    }
+
+    
+    
+    /**
+     *
+     * @return
+     */
+    public String getNom() {
+        return nom;
+    }
+}
